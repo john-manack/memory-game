b2007c56b3d00bc22aa9995bcb8ac5ed8c345375
John Manack <jmnck28@gmail.com>
2021-02-25 15:28:58 -0500

    cards be flippin

--- src/components/MemoryCard.jsx
+++ src/components/MemoryCard.jsx
@@ -2,23 +2,31 @@ import React, { Component } from 'react';
 import './MemoryCard.css';
 
 class MemoryCard extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isFlipped: false
+        };
+    }
+    
     render() {
+        let memoryCardInnerClass = this.state.isFlipped ? "MemoryCardInner flipped" : "MemoryCardInner";
         return (
             <div className="MemoryCard" onClick={this.clickHandler()}>
-                <div className="MemoryCardInner">
+                <div className={memoryCardInnerClass}>
                     <div className="MemoryCardBack">
                         <img src="https://www.digitalcrafts.com/img/digitalcrafts-logo-white-y.png" alt="dc logo"></img>
                     </div>
-                    <div className="MemoryCardFront">
-                        ∆
-                    </div>
+                    <div className="MemoryCardFront">∆</div>
                 </div>
             </div>
         );
     }
 
     clickHandler() {
-        return () => alert('card clicked');
+        return () => this.setState({
+            isFlipped: !this.state.isFlipped
+        });
     }
 }
 
